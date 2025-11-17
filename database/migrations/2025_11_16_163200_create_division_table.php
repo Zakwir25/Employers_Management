7bf7c3554b0faf34da3dc21948ec9521bbcaec31
Zakwir25 <108886009+Zakwir25@users.noreply.github.com>
2025-11-17 12:16:16 +0700

    Add position_role column to division table migration

--- database/migrations/2025_11_16_163200_create_division_table.php
+++ database/migrations/2025_11_16_163200_create_division_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('division', function (Blueprint $table) {
             $table->id();
+            $table->string('position_role');
             $table->string('division');
             $table->timestamps();
         });

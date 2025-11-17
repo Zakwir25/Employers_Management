1b81e01953c53886be387ccc235f22c8d7a538e3
Zakwir25 <108886009+Zakwir25@users.noreply.github.com>
2025-11-17 21:45:38 +0700

    Update users and employees table migrations to enforce unique constraints and correct data types

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('username');
             $table->string('email')->unique();
-            $table->string('nik')->unique();
+            $table->string('nik', 20)->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
--- database/migrations/2025_11_17_083645_create_employees_table.php
+++ database/migrations/2025_11_17_083645_create_employees_table.php
@@ -12,19 +12,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('employees', function (Blueprint $table) {
-            $table->id();
+            $table->ulid('id');
             $table->interger('user_id');
-            $table->string('nik');
+            $table->string('nik')->unique();
             $table->string('name');
-            $table->string('email');
-            $table->string('Gender');
-            $table->string('position_role');
-            $table->string('division');
+            $table->string('email')->unique()->nullable(false);
+            $table->string('Gender', ['Laki-laki','Perempuan']);
+            $table->enum('position_role', ['Staff','Admin','Supervisor','Manager','Intern']);
+            $table->enum('division', ['HRD','Finance','IT','Marketing','Operation','GA']);
             $table->date('joining_date');
-            $table->string('phone_number', 15);
-            $table->date('birth_date');
-            $table->string('address');
-            $table->string('status');
+            $table->string('phone_number', 15)->unique();
+            $table->date('birth_date')->unique();
+            $table->string('address')->nullable();
+            $table->enum('status', ['Active','Inactive','Resigned','On Leave'])->default('Active');
             $table->decimal('salary', 10, 2);
             $table->timestamps();
         });
